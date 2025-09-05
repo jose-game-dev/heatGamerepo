37e3e333f6dd52f026cc7f1469c2e7eb877e21c1
Jose-web-dev <jose.colon193@upr.edu>
2025-09-05 19:57:55 -0400

    update 0.2
    Added death animation for flying_mob

--- Scenes/flying_mob.gd
+++ Scenes/flying_mob.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 var speed: float = 100
 
 func _ready():
+	input_pickable = true
 	velocity = _random_up_direction() * speed
 	
 func _random_up_direction():
@@ -16,3 +17,19 @@ func _physics_process(delta):
 	
 	if col:
 		velocity = velocity.bounce(col.get_normal())
+		
+	_check_direction()
+	
+func _check_direction():
+	if velocity.x <0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+		
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if event.is_action_pressed("shoot"):
+		#when we click the demon it gets destroyed
+		queue_free()
+		
