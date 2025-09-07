6d3808d460fe98fdea61f124baa65d5990615cce
Jose-web-dev <jose.colon193@upr.edu>
2025-09-06 22:01:02 -0400

    Added main menu

--- Scenes/field.gd
+++ Scenes/field.gd
@@ -1,29 +1,102 @@
+#extends Node2D
+#@onready var bullet_node: Label = $CanvasLayer/bullet
+#@onready var Round_node: Label = $CanvasLayer/round
+#@onready var score_node: Label = $CanvasLayer/score
+#
+#
+#var bullet_count = 3:
+	#set(value):
+		#if value>=0:
+			#bullet_count = value
+			#bullet_node.text = str(value)
+		#else:
+			#_lose()
+			#
+#var Round = 1:
+	#set(value):
+		#Round = value
+		#Round_node.text = str(value)
+		#
+#var score = 0:
+	#set(value):
+		#score = value
+		#score_node.text = str(value)
+#
+#@export var flying_mob_node: PackedScene
+#var flying_mob
+#
+#func _ready():
+	#_default_display()
+	#_spawn_enemy()
+#
+#func _default_display():
+	#bullet_node.text = str(bullet_count)
+	#Round_node.text = str(Round)
+	#score_node.text = str(score)
+	#
+	#
+#func _spawn_enemy():
+	#if flying_mob != null:
+		#await flying_mob.tree_exited
+	#flying_mob = flying_mob_node.instantiate()
+	#
+	#flying_mob.speed = Round * 30
+	#flying_mob.position = Vector2(160,90)
+	#flying_mob.next_round.connect(_on_next_round)
+	#flying_mob.call_deferred("set_name", "flying mob")
+	#
+	#get_tree().current_scene.add_child(flying_mob)
+	#
+#func _on_next_round():
+	#score += Round * 100
+	#Round +=1
+	#bullet_count = 5
+	#
+	#_spawn_enemy()
+	#
+##shooting count
+#
+#func _input(event):
+	#if event.is_action_pressed("shoot"):
+		#bullet_count -=1
+#
+#func _lose():
+	#if $Timer.is_stopped():
+		#find_child("Laugh_mob").dog_laugh()
+		#find_child("flying_mob", false, false).input_pickable = false
+		#$Timer.start()
+#
+#func _on_timer_timeout() -> void:
+	#get_tree().reload_current_scene()
+
 extends Node2D
 @onready var bullet_node: Label = $CanvasLayer/bullet
 @onready var Round_node: Label = $CanvasLayer/round
 @onready var score_node: Label = $CanvasLayer/score
 
-
-var bullet_count = 3:
+var bullet_count = 6:
 	set(value):
-		if value>=0:
+		if value >= 0:
 			bullet_count = value
 			bullet_node.text = str(value)
 		else:
 			_lose()
-			
+
 var Round = 1:
 	set(value):
 		Round = value
 		Round_node.text = str(value)
-		
+
 var score = 0:
 	set(value):
 		score = value
 		score_node.text = str(value)
 
 @export var flying_mob_node: PackedScene
-var flying_mob
+
+var alive_enemies: int = 0
+var next_round_enemy_count: int = 3 
+var spawn_positions := [Vector2(160,90), Vector2(200,100), Vector2(120,110), Vector2(180,120)]
 
 func _ready():
 	_default_display()
@@ -33,37 +106,40 @@ func _default_display():
 	bullet_node.text = str(bullet_count)
 	Round_node.text = str(Round)
 	score_node.text = str(score)
-	
-	
+
 func _spawn_enemy():
-	if flying_mob != null:
-		await flying_mob.tree_exited
-	flying_mob = flying_mob_node.instantiate()
-	
-	flying_mob.speed = Round * 30
-	flying_mob.position = Vector2(160,90)
-	flying_mob.next_round.connect(_on_next_round)
-	flying_mob.call_deferred("set_name", "flying mob")
-	
-	get_tree().current_scene.add_child(flying_mob)
-	
-func _on_next_round():
-	score += Round * 100
-	Round +=1
-	bullet_count = 5
-	
-	_spawn_enemy()
-	
-#shooting count
+	alive_enemies = next_round_enemy_count
+	for i in range(next_round_enemy_count):
+		var enemy = flying_mob_node.instantiate()
+		enemy.speed = 100 
+		enemy.position = spawn_positions[i % spawn_positions.size()]
+		enemy.next_round.connect(_on_enemy_dead)
+		get_tree().current_scene.add_child(enemy)
+
+func _on_enemy_dead():
+	alive_enemies -= 1
+	score += 10
+	if alive_enemies <= 0:
+		if next_round_enemy_count == 3:
+			next_round_enemy_count = 4
+		else:
+			next_round_enemy_count = 3
+		
+		Round += 1
+		bullet_count = 6
+		await get_tree().create_timer(1).timeout
+		_spawn_enemy()
 
 func _input(event):
 	if event.is_action_pressed("shoot"):
-		bullet_count -=1
+		bullet_count -= 1
 
 func _lose():
 	if $Timer.is_stopped():
 		find_child("Laugh_mob").dog_laugh()
-		find_child("flying_mob", false, false).input_pickable = false
+		for enemy in get_tree().get_current_scene().get_children():
+			if "input_pickable" in enemy:
+				enemy.input_pickable = false
 		$Timer.start()
 
 func _on_timer_timeout() -> void:
--- Scenes/flying_mob.gd
+++ Scenes/flying_mob.gd
@@ -1,53 +1,139 @@
+#extends CharacterBody2D
+#
+#@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+#@onready var animation_player: AnimationPlayer = $AnimationPlayer
+#@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+#
+#var speed: float = 100
+#
+#signal next_round
+#
+#func _ready():
+	#input_pickable = true
+	#velocity = _random_up_direction() * speed
+	#
+#func _random_up_direction():
+	#var x = deg_to_rad(randf_range(0,180))
+	#return Vector2(cos(x), sin(x)).normalized()
+	#
+#func _physics_process(delta):
+	#var col = move_and_collide(velocity * delta)
+	#
+	#if col:
+		#velocity = velocity.bounce(col.get_normal())
+		#
+	#_check_direction()
+	#
+#func _check_direction():
+	#if velocity.x <0:
+		#sprite.flip_h = true
+	#else:
+		#sprite.flip_h = false
+		#
+#
+#
+#func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
+	#if event.is_action_pressed("shoot"):
+		##when we click the demon it gets destroyed
+		#death()
+		#
+#
+#func death():
+	#velocity = Vector2.ZERO
+	#animation_player.play("death")
+	##collision_shape_2d.disabled
+	##wait one second before applying Vy (dramatic effect)
+	#await get_tree().create_timer(1).timeout
+	#velocity = Vector2.DOWN * 100
+	##animation_player.reset_on_save/
+#
+#
+#func _on_screen_notifier_2d_screen_exited():
+	#next_round.emit()
+	#queue_free()
+
+
 extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-var speed: float = 100
+var speed: float = 10
+var change_animation_timer := 0.0
+var change_interval := 5
 
 signal next_round
 
+var area_margin := 10.0
+
 func _ready():
 	input_pickable = true
-	velocity = _random_up_direction() * speed
-	
-func _random_up_direction():
+	velocity = _random_direction() * speed
+	sprite.play("fly_forward")
+
+func _random_direction():
 	var x = deg_to_rad(randf_range(0,180))
 	return Vector2(cos(x), sin(x)).normalized()
-	
-func _physics_process(delta):
-	var col = move_and_collide(velocity * delta)
-	
-	if col:
-		velocity = velocity.bounce(col.get_normal())
-		
-	_check_direction()
-	
-func _check_direction():
-	if velocity.x <0:
-		sprite.flip_h = true
-	else:
-		sprite.flip_h = false
-		
 
+func _physics_process(delta):
+	_move_enemy(delta)
+	_check_bounds()
+	_check_direction()
+	_update_animation(delta)
+
+func _move_enemy(delta):
+	if sprite.animation == "going_up":
+		position.y -= speed * delta
+	else:
+		position += velocity * delta
+
+func _check_bounds():
+	var viewport = get_viewport_rect()
+	
+	if sprite.animation == "going_up":
+		if position.y < area_margin:
+			sprite.play("fly_forward")
+			velocity.y = abs(velocity.y)
+		elif position.y > viewport.size.y - area_margin:
+			sprite.play("fly_forward")
+			velocity.y = -abs(velocity.y)
+	else:
+		if position.x < area_margin:
+			velocity.x = abs(velocity.x)
+			if randi() % 2 == 0:
+				sprite.play("going_up")
+		elif position.x > viewport.size.x - area_margin:
+			velocity.x = -abs(velocity.x)
+			if randi() % 2 == 0:
+				sprite.play("going_up")
+		if position.y < area_margin:
+			velocity.y = abs(velocity.y)
+			sprite.play("fly_forward")
+		elif position.y > viewport.size.y - area_margin:
+			velocity.y = -abs(velocity.y)
+			sprite.play("fly_forward")
+
+func _check_direction():
+	sprite.flip_h = velocity.x < 0
+
+func _update_animation(delta):
+	change_animation_timer += delta
+	if change_animation_timer >= change_interval:
+		change_animation_timer = 0
+		if sprite.animation != "death":
+			if randi() % 2 == 0:
+				sprite.play("going_up")
+			else:
+				sprite.play("fly_forward")
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if event.is_action_pressed("shoot"):
-		#when we click the demon it gets destroyed
 		death()
-		
 
 func death():
 	velocity = Vector2.ZERO
 	animation_player.play("death")
-	#collision_shape_2d.disabled
-	#wait one second before applying Vy (dramatic effect)
 	await get_tree().create_timer(1).timeout
-	velocity = Vector2.DOWN * 100
-	#animation_player.reset_on_save/
-
-
-func _on_screen_notifier_2d_screen_exited():
-	next_round.emit()
+	next_round.emit()  # <- emitir señal al morir
 	queue_free()
